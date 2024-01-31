8e1a1492a89ddc78333cbee012495ae981d70866
Adrienne Estrella <60564656+adriennereyes@users.noreply.github.com>
2024-01-31 18:26:58 -0500

    Successfully implemented creating a quote and creating a delivery using quote id

--- lula-ecommerce-store/src/api/uber/delivery/route.ts
+++ lula-ecommerce-store/src/api/uber/delivery/route.ts
@@ -6,31 +6,3 @@ export async function GET(req: MedusaRequest, res: MedusaResponse) {
       testing: req.headers
     });
 }
-
-export async function POST(req: MedusaRequest, res: MedusaResponse) {
-    switch (req.body.type) {
-        case "create":
-            createDelivery(req, res);
-            break;
-        case "update":
-            updateDelivery(req, res);
-            break;
-        case "cancel":
-            cancelDelivery(req, res);
-            break;
-        default:
-            break;
-    }
-}
-
-const createDelivery =  function(req: MedusaRequest, res: MedusaResponse): MedusaResponse {
-    return res
-}
-
-const updateDelivery =  function(req: MedusaRequest, res: MedusaResponse): MedusaResponse {
-    return res
-}
-
-const cancelDelivery =  function(req: MedusaRequest, res: MedusaResponse): MedusaResponse {
-    return res
-}
