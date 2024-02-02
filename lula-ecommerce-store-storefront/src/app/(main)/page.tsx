1c64bde46ba2bafb927742e0d180389206b45e46
civinko <57294125+civinko@users.noreply.github.com>
2024-02-02 01:03:27 -0500

    package.json added requirement for meilisearch,
    package.json added requirement for meilisearch,

--- lula-ecommerce-store-storefront/src/app/(main)/page.tsx
+++ lula-ecommerce-store-storefront/src/app/(main)/page.tsx
@@ -6,7 +6,7 @@ import FeaturedProducts from "@modules/home/components/featured-products"
 import Hero from "@modules/home/components/hero"
 
 export const metadata: Metadata = {
-  title: "Medusa Next.js Starter Template",
+  title: "Lula store template",
   description:
     "A performant frontend ecommerce starter template with Next.js 14 and Medusa.",
 }
@@ -56,7 +56,7 @@ export default async function Home() {
 
   return (
     <>
-      <Hero />
+      {/*<Hero />*/}
       <div className="py-12">
         <ul className="flex flex-col gap-x-6">
           <FeaturedProducts collections={collections} />
