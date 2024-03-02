40333b146bfdede15b0ecb3e15d507c1857bce21
Abby <ada77@drexel.edu>
2024-03-02 16:01:22 -0500

    added text and email notifs

---
+++ lula-ecommerce-store/src/types.ts
@@ -0,0 +1,42 @@
+import {EmailData} from '@sendgrid/helpers/classes/email-address'
+
+/** @exampledata https://docs.medusajs.com/plugins/notifications/sendgrid */
+
+export interface Template {
+  /** The dynamic template id. */
+  id: string
+  /** The subject of the email. */
+  subject?: string
+}
+
+export interface Templates {
+  /** Template to be sent to the customer when they place a new order. */
+  order_placed_template?: Template
+  order_canceled_template?: Template
+  order_shipped_template?: Template
+  order_return_requested_template?: Template
+  order_items_returned_template?: Template
+  claim_shipment_created_template?: Template
+  swap_created_template?: Template
+  swap_shipment_created_template?: Template
+  swap_received_template?: Template
+  gift_card_created_template?: Template
+  customer_password_reset_template?: Template
+  user_password_reset_template?: Template
+  medusa_restock_template?: Template
+  order_refund_created_template?: Template
+  // add any other templates here
+  [key: string]: Template | undefined
+}
+
+export interface PluginOptions {
+  api_key: string
+  from: string
+  templates: Templates;
+  /** BCC email address to send to when an order is placed. */
+  orderPlacedBcc?: EmailData|EmailData[]
+  /** locale as key example de-DE */
+  localization: {
+    [key: string]: Templates;
+  }
+}
