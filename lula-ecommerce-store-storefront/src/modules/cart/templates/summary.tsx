164dac99ca3d6658028a017c877e54021f809c5c
Adrienne Estrella <60564656+adriennereyes@users.noreply.github.com>
2024-03-05 18:05:14 -0500

    Refactored how we're handling tipping

--- lula-ecommerce-store-storefront/src/modules/cart/templates/summary.tsx
+++ lula-ecommerce-store-storefront/src/modules/cart/templates/summary.tsx
@@ -1,26 +1,28 @@
-"use client"
-
 import { Button, Heading } from "@medusajs/ui"
 import Link from "next/link"
 
 import CartTotals from "@modules/common/components/cart-totals"
 import Divider from "@modules/common/components/divider"
 import { CartWithCheckoutStep } from "types/global"
-import DiscountCode from "@modules/checkout/components/discount-code"
+import { retrieveCart } from "../actions"
 
 type SummaryProps = {
   cart: CartWithCheckoutStep
 }
 
-const Summary = ({ cart }: SummaryProps) => {
+const Summary = async ({ cart }: SummaryProps) => {
+  const cartWithTip = await retrieveCart().then((cartWithTip) => cartWithTip)
+
+  if (!cartWithTip) {
+    return null
+  }
   return (
     <div className="flex flex-col gap-y-4">
       <Heading level="h2" className="text-[2rem] leading-[2.75rem]">
         Summary
       </Heading>
-      <DiscountCode cart={cart} />
       <Divider />
-      <CartTotals data={cart} />
+      <CartTotals data={cartWithTip} />
       <Link href={"/checkout?step=" + cart.checkout_step}>
         <Button className="w-full h-10">Go to checkout</Button>
       </Link>
