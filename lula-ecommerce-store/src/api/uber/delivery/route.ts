1a521c890d16fcc98062f72df18f03cfdaf950c3
Adrienne Estrella <60564656+adriennereyes@users.noreply.github.com>
2024-03-10 11:48:31 -0400

    Added api routes to get a delivery by delivery id for DoorDash and uber

--- lula-ecommerce-store/src/api/uber/delivery/route.ts
+++ lula-ecommerce-store/src/api/uber/delivery/route.ts
@@ -1,8 +1,21 @@
 import { MedusaRequest, MedusaResponse } from "@medusajs/medusa";
+import axios from "axios";
 
 export async function GET(req: MedusaRequest, res: MedusaResponse) {
-    res.json({
-      message: "Hello world",
-      testing: req.headers
-    });
+  const url = `https://api.uber.com/v1/customers/${process.env.UBER_CUSTOMER_ID}/deliveries/${req.body.deliveryId}`;
+  const headers = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: req.headers.authorization,
+    },
+  };
+
+  axios.get(url, headers)
+    .then(response => {
+      console.log(response.data);
+      res.json(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
 }
